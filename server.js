d66ec1574b3552befa11cfc3cbe4ef2c6ec5fac4
Ed <edcheung@umich.edu>
2016-01-25 21:39:05 +0000

    added mongolab URI

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var mongo = require('mongodb').MongoClient;
 
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/imgsearch', function (err, db) {
+mongo.connect('mongodb://heroku_76c7rxjn:25fg9ffd042clbdvq84e339rnj@ds049935.mongolab.com:49935/heroku_76c7rxjn', function (err, db) {
     if (err) {
         throw new Error('Database failed to connect.');
     } else {
